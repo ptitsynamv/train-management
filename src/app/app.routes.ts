f1241d8b6ac69ab41c2424055ab0e5ea165ed569
ptitsynamv <ptitsynamv@gmail.com>
2025-07-01 12:57:25 +0300

    Add 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { DetailsComponent } from './components/details/details.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'train/:id', component: DetailsComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
